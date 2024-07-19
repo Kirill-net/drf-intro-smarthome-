86559c4f9eb699d337a9bcf0644cce016d6cac09
SMRT <Kirill1309913099@yandex.ru>
2024-07-19 17:33:10 +0300

    correct models

--- measurement/models.py
+++ measurement/models.py
@@ -12,11 +12,11 @@ class Sensor(models.Model):
 
 class Measurement(models.Model):
 
-    temperature = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    changed_at = models.DateTimeField(auto_now=True)
     sensor = models.ForeignKey(
         Sensor,
         on_delete=models.CASCADE,
         related_name='measurements'
         )
+    temperature = models.DecimalField(decimal_places=1, max_digits=4)
+    created_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateTimeField(auto_now=True)
